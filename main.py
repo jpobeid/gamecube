5e9e609da917339e8fa4e16559c5eb64e120a94d
JP Obeid <jpobeid91@gmail.com>
2022-08-04 14:40:32 -0400

    Improved main to be directly controlled to GPIOs

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 import usb_reader as usb_gc
 import controller as gc
-import game
+import time
+import RPi.GPIO as GPIO
+import usb.core
+
+GPIO.setmode(GPIO.BOARD)
+
+PIN_RELAY = 37
+GPIO.setup(PIN_RELAY, GPIO.OUT)
 
 endpoint = usb_gc.get_controller_endpoint()
 
+def toggle(pin, signal):
+    if signal and GPIO.input(pin) == 0:
+        print(f'Turning on pin {pin}...')
+        GPIO.output(pin, True)
+    elif not signal and GPIO.input(pin) == 1:
+        print(f'Turning off pin {pin}...')
+        GPIO.output(pin, False)
+
 while True:
-    raw_bytes = endpoint.read(8)[:-1] # Only 7 bytes are pertinent, last is always 0
+    time.sleep(0.05)
+    try:
+        raw_bytes = endpoint.read(8)[:-1] # Only 7 bytes are pertinent, last is always 0
+    except usb.core.USBError as e:
+        print(e)
+        print('Cleaning up...')
+        GPIO.cleanup()
+        break
     controller_output = gc.interpret(raw_bytes)
-    game.play(controller_output)
+    signal_A = 'A' in  controller_output.get('Main')
+    signal_B = 'B' in controller_output.get('Main')
+    signal_C_up = controller_output.get('C-stick')[-1] > 0
+    signal_C_down = controller_output.get('C-stick')[-1] < 0
+    toggle(PIN_RELAY, signal_A)
+    signal_close = 'Home' in controller_output.get('Center')
+    if signal_close:
+        GPIO.cleanup()
+        print('Shutting down...')
+        break
